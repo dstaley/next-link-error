e60623d076fb7b45b58661aefd5072ad98ff9819
Dylan Staley <staleydyla.n@gmail.com>
2017-11-29 16:28:44 -0800

    Initial commit

---
+++ components/Footer.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Link from 'next/router';
+
+export default () => (
+    <a href="/">
+        <a>Home</a>
+    </a>
+);
---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Footer from '../components/Footer';
+
+export default () => (
+    <div>
+        <h1>Hello!</h1>
+        <Footer />
+    </div>
+);
